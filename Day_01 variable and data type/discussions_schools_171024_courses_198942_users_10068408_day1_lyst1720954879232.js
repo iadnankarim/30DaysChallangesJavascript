5f0d77718c5254e667d9dacf521809975dd89185
iadnankarim <adnankarim725@gmail.com>
2024-07-15 02:55:08 +0500

    day_01 variable and datatype

---
+++ Day_01 variable and data type/discussions_schools_171024_courses_198942_users_10068408_day1_lyst1720954879232.js
@@ -0,0 +1,34 @@
+// Variables and datatypes
+
+//  var num=10;
+//  console.log(num);
+//  let name ="manikant yadav";
+//  console.log(name);
+
+//  const val=true;
+//  console.log(val);
+
+// let number=10;
+// console.log(number,typeof number);
+// let string="Manikant yadav";
+// console.log(typeof string);
+// let boolean= true;
+// console.log(typeof boolean);
+// let obj={
+//     username:"Manikant yadav",
+//     age: 22
+// }
+// console.log(typeof obj);
+// let arr=[1,23,43,22];
+
+// console.log(typeof arr);
+
+// let val=10;
+// console.log(val);
+// val=55;
+// console.log(val);
+
+// const val=10;
+// console.log(val);
+// val=55;
+// console.log(val);
